e7586d4b74893e598b510f182a6b6fca2e568b80
RSV2255 <rushivutukuri@gmail.com>
2024-11-15 14:01:30 +0530

    updated server

--- routes/project.js
+++ routes/project.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
-
 module.exports = (db) => {
        router.get('/fetchServices/:projectId', async (req, res) => {
         const projectId = req.params.projectId;
+        const query = `
+        SELECT s.serviceName, COUNT(ps.serviceId)* 25 AS percentage 
+        FROM vendorServices s LEFT JOIN projectServices ps 
+        WHERE s.servicesId = ps.serviceId AND ps.projectId = ? 
+        GROUP BY ps.serviceId 
+        ORDER BY ps.serviceId DESC`
         try {
-            const services = await db.all('SELECT * FROM projectServices WHERE projectId = ?', [projectId]);
+            const services = await db.all(query, [projectId]);
             if (services.length > 0) {
                 res.json({ success: true, services });
             } else {
