57e789d05171e1bdca829c040e9e68fe6f49cf61
RSV2255 <rushivutukuri@gmail.com>
2024-11-19 18:30:25 +0530

    ___

--- routes/project.js
+++ routes/project.js
@@ -7,7 +7,7 @@ module.exports = (db) => {
         router.get('/fetchServices/:projectId', async (req, res) => {
         const projectId = req.params.projectId;
         const query = `
-        SELECT s.serviceName, ps.createdAt, COUNT(ps.serviceId) * 25 AS percentage 
+        SELECT ps.serviceId, s.serviceName, ps.createdAt, COUNT(ps.serviceId) * 25 AS percentage 
         FROM vendorServices s LEFT JOIN projectServices ps 
         WHERE s.servicesId = ps.serviceId AND ps.projectId = ? 
         GROUP BY ps.serviceId 
@@ -24,6 +24,25 @@ module.exports = (db) => {
             console.error('Error fetching services:', error);
             res.status(500).json({ success: false, message: 'Internal server error' });
         }
+        });
+        router.get('/fetchServiceStatus/:serviceId', async (req, res) => {
+            const serviceId = req.params.serviceId;
+            const query = `
+            SELECT * FROM projectServices 
+            WHERE serviceId = ?
+            ORDER BY createdAt
+            `
+            try {
+                const services = await db.all(query, [serviceId]);
+                if (services.length > 0) {
+                    res.json({ success: true, services });
+                } else {
+                    res.status(404).json({ success: false, message: 'No services found' });
+                }
+            } catch (error) {
+                console.error('Error fetching services:', error);
+                res.status(500).json({ success: false, message: 'Internal server error' });
+            }
         });
         return router;
 }
